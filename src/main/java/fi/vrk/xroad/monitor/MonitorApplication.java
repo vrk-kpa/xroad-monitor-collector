24827ee023bfc7d0f24f709af729c013a97ec501
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-05-11 13:51:00 +0300

    PVAYLADEV-752: Add dependency to Spring Boot

---
+++ src/main/java/fi/vrk/xroad/monitor/MonitorApplication.java
@@ -0,0 +1,15 @@
+package fi.vrk.xroad.monitor;
+
+/**
+ * Main class of the application
+ */
+public class MonitorApplication {
+
+    /**
+     * Entry point
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hellow");
+    }
+}
