2fbc7ca4864d7cada69f35bbbec00f47037a19e4
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-08-11 10:59:13 +0300

    PVAYLADEV-824: Add transport client configuration

--- src/main/java/fi/vrk/xroad/monitor/configuration/ApplicationConfiguration.java
+++ src/main/java/fi/vrk/xroad/monitor/configuration/ApplicationConfiguration.java
@@ -26,6 +26,11 @@ import akka.actor.ActorSystem;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import fi.vrk.xroad.monitor.extensions.SpringExtension;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -35,6 +40,9 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
@@ -73,41 +81,26 @@ public class ApplicationConfiguration {
         return ConfigFactory.load();
     }
 
-
     /**
      *
      * @return client
      */
-//    @Bean
-//    public Client client() {
-//        try {
-//            final Path tmpDir = Files.createTempDirectory(Paths.get(System.getProperty("java.io.tmpdir")),
-//                "elasticsearch_data");
-//
-//            final Settings.Builder elasticsearchSettings =
-//                Settings.settingsBuilder().put("http.enabled", "false")
-//                    .put("path.data", tmpDir.toAbsolutePath().toString())
-//                    .put("path.home", "/home/ilkka");
-//
-//            Client client = new NodeBuilder()
-//                .local(true)
-//                .settings(elasticsearchSettings)
-//                .node()
-//                .client();
-//            return client;
-//
-//            // @formatter:on
-//        } catch (final IOException ioex) {
-//            throw new RuntimeException();
-//        }
-//    }
+    @Bean
+    public Client client() throws UnknownHostException {
+        Client client = TransportClient.builder().build()
+            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+        return client;
+    }
 
     /**
      *
      * @return operations
      */
     @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
+    public ElasticsearchOperations elasticsearchTemplate() throws UnknownHostException {
+        // external Elasticsearch server
+        return new ElasticsearchTemplate(client());
+        // embedded Elasticsearch server
+        // return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
     }
 }
