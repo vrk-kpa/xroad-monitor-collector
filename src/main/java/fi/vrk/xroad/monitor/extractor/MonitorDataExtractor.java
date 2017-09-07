ac48b8ff3be3aacb8011cc82b1a14a70c987fed8
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-09-07 08:34:16 +0300

    Add logging

--- src/main/java/fi/vrk/xroad/monitor/extractor/MonitorDataExtractor.java
+++ src/main/java/fi/vrk/xroad/monitor/extractor/MonitorDataExtractor.java
@@ -79,6 +79,8 @@ public class MonitorDataExtractor {
      */
     public String makeRequest(String xmlRequest) {
 
+        log.info("OBJECT: {} THREAD: {}", System.identityHashCode(rt), Thread.currentThread().getId());
+
         rt.getMessageConverters().add(new Jaxb2RootElementHttpMessageConverter());
         rt.getMessageConverters().add(new StringHttpMessageConverter());
 
