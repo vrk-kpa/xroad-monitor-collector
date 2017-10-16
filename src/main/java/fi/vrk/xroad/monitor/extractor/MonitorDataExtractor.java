b797ec492a69c48e2838e4610cc22046e7a231f1
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-10-16 16:15:57 +0300

    PVAYLADEV-975: Keystore for RestTemplate

--- src/main/java/fi/vrk/xroad/monitor/extractor/MonitorDataExtractor.java
+++ src/main/java/fi/vrk/xroad/monitor/extractor/MonitorDataExtractor.java
@@ -25,15 +25,29 @@ package fi.vrk.xroad.monitor.extractor;
 import fi.vrk.xroad.monitor.parser.SecurityServerInfo;
 import fi.vrk.xroad.monitor.util.MonitorCollectorPropertyKeys;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 /**
  * Handler for extractor requestBuilder, responseParser and parsing
@@ -53,11 +67,26 @@ public class MonitorDataExtractor {
     @Autowired
     private MonitorDataResponseParser responseParser;
 
+    private static String keyStoreFile = "/etc/xroad/xroad-monitor-collector/keystore";
+    private static String keyStorePassword = "foobar";
+
+
     /**
      * Constructor
      */
-    public MonitorDataExtractor() {
-        rt = new RestTemplate();
+    public MonitorDataExtractor() throws KeyStoreException, IOException, CertificateException,
+        NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(new FileInputStream(new File(keyStoreFile)), keyStorePassword.toCharArray());
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+            new SSLContextBuilder()
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                .loadKeyMaterial(keyStore, keyStorePassword.toCharArray())
+                .build(),
+            NoopHostnameVerifier.INSTANCE);
+        HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        rt = new RestTemplate(requestFactory);
         rt.getMessageConverters().add(new Jaxb2RootElementHttpMessageConverter());
         rt.getMessageConverters().add(new StringHttpMessageConverter());
     }
