af2a9e6fba341c1f8b9fabe35cce91ec948bbc1d
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-10-25 13:22:54 +0300

    PVAYLADEV-976: Add scripts for building development and release packages

---
+++ build_release_packages.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker build -t collector-rpm packages/xroad-monitor-collector/docker
+docker run -v $PWD/..:/workspace  -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro collector-rpm /workspace/xroad-monitor-collector/build_rpm.sh -release
