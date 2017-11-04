fc6c16749a4080a56e19f8bc81ee59227dd7ce4a
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-11-04 20:04:50 +0200

    PVAYLADEV-976: Update release package build

--- build_release_packages.sh
+++ build_release_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t collector-rpm packages/xroad-monitor-collector/docker
-docker run -v $PWD/..:/workspace  -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro collector-rpm /workspace/xroad-monitor-collector/build_rpm.sh -release
+docker run -v $PWD/..:/workspace  -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u):$(id -g) collector-rpm /workspace/xroad-monitor-collector/build_rpm.sh -release
